f0c33e38f3767420eef9774a2b5e154dc1d8827e
bamshak9 <b.fwangyil@alustudent.com>
2025-07-22 17:24:48 +0100

    added tasks 4 through 6

---
+++ task5.py
@@ -0,0 +1,12 @@
+playlist = ["Song A", "Song B", "Song C"]
+playlist[1] = "Song D"
+print(playlist)
+
+playlist.append("song E")
+print(playlist)
+
+playlist.insert(0, "intro")
+print(playlist)
+
+print(playlist)
+
---
+++ task6.py
@@ -0,0 +1,8 @@
+desks = []
+student1 = input("Enter Student Name: ")
+student2 = input("Enter Student Name: ")
+student3 = input("Enter Student Name: ")
+desks = [student1, student2, student3]
+desks[1] = input("Enter new student here: ")
+desks.insert(1, input("Enter new student here: "))
+print(desks)
