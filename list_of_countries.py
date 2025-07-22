2b0b15919e03b631c865175a6e5e674697ecd06f
bamshak9 <b.fwangyil@alustudent.com>
2025-07-22 15:11:03 +0100

    added task 1

---
+++ list_of_countries.py
@@ -0,0 +1,7 @@
+countries = ["Austria", "Malawi", "Russia", "Nigeria", "USA", "Japan"]
+print(countries[4])
+print(countries[-1])
+print(countries[1:-1])
+print(countries[:-1])
+
+
---
+++ task1.py
@@ -0,0 +1,10 @@
+favorite_movies = ["Naruto", "Gintama", "Steins gate", "Re:Zero", "Kaguya sama"]
+numbers = [1, 2, 3, 4, 5, 6 , 7, 8, 9, 10]
+mixed = ["Bamshak", "24", False, "Male", "6'0","Orange"]
+provisions = ["Indomie", "Rice", "Onions", "Maggi", "Biscuits", "Sugar"]
+
+print(favorite_movies)
+print(numbers)
+print(mixed)
+print(provisions)
+
