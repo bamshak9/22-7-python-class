69d05eaac193a152e1e0b799461fd9cf26d0efcb
bamshak9 <b.fwangyil@alustudent.com>
2025-07-22 16:18:23 +0100

    added task 3

---
+++ task2.py
@@ -0,0 +1,5 @@
+code = ["x", "H", "e", "z", "l", "l", "!", "p", "o", "- ", "n", " ", "W", "@", "r","d","o","#"]
+print(code[1] + code[2] + code[4] + code[4] + code[8])
+print(code[12]+ code[8] + code[14] +code[4] + code[-3])
+print(code[::-1])
+
---
+++ task3.py
@@ -0,0 +1,8 @@
+grid = [
+        ["The", "sky", "is"],
+        ["full", "of", "stars"],
+        ["shining", "bright", "tonight"]
+]
+print(grid[0][0], grid[0][1])
+print(grid[0][0], grid[1][2],"are", grid[2][0])
+print(grid[1][::-1])
