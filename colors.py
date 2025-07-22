c6bf9d0fd773d7be122e70142e5b31761248b22d
bamshak9 <b.fwangyil@alustudent.com>
2025-07-22 14:18:32 +0100

    added colors and country

---
+++ colors.py
@@ -0,0 +1,8 @@
+first_favourite_colour = "Blue"
+second_favourite_colour = "Red"
+third_favourite_colour = "Yellow"
+fourth_favourite_colour = "Green"
+fifth_favourite_colour = "Gray"
+print(first_favourite_colour)
+print(fifth_favourite_colour)
+
---
+++ country.py
@@ -0,0 +1,9 @@
+country1 = "Nigeria"
+country2 = "Ghana"
+country3 = "Benin"
+country4 = "Mauritius"
+country5 = "Japan"
+country6 = "United States of America"
+
+print(country5)
+
